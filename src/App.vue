1be5c57582c60657ef324aec3431213b5066c0d6
Oleg <olegik-n@yandex.com>
2023-04-05 01:15:21 +0300

    Added header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,63 @@
 <template>
 	<v-app id="app">
-		<nav>
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link> |
-			<router-link to="/form">Add new article</router-link>
-		</nav>
-		<div class="error-button" v-show="isFailed">
-			<p v-show="isCanceled">
-				Загрузка отменена!
-			</p>
-			<p v-show="!isCanceled">
-				Загрузка не удалась...
-			</p>
-			<button @click="fetchArticles">Загрузить</button>
-		</div>
-		<div v-show="isRequested">
-			<img src="loading.gif" alt="Loading...">
-			<button @click="cancelLoading">Отменить</button>
-		</div>
-		<router-view v-show="isSucceeded" />
+		<v-navigation-drawer app v-model="drawer">
+			<v-list>
+				<v-list-item>
+					<v-list-item-content>
+						<v-list-item-title class="text-h6">Menu</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+			<v-divider>
+
+			</v-divider>
+			<v-list dense nav>
+				<v-list-item link to="/">
+					<v-list-item-icon>
+						<v-icon>mdi-home-circle</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>Home</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link to="/about">
+					<v-list-item-icon>
+						<v-icon>mdi-information</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>About</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-item link to="/form">
+					<v-list-item-icon>
+						<v-icon>mdi-plus-box</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>Add your article</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-app-bar color="blue" elevation="3" app>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-app-bar-title>VueJS</v-app-bar-title>
+		</v-app-bar>
+		<v-main>
+			<div class="error-button" v-show="isFailed">
+				<p v-show="isCanceled">
+					Загрузка отменена!
+				</p>
+				<p v-show="!isCanceled">
+					Загрузка не удалась...
+				</p>
+				<button @click="fetchArticles">Загрузить</button>
+			</div>
+			<div v-show="isRequested">
+				<img src="loading.gif" alt="Loading...">
+				<button @click="cancelLoading">Отменить</button>
+			</div>
+			<router-view v-show="isSucceeded" />
+		</v-main>
 	</v-app>
 </template>
 
@@ -29,6 +68,9 @@ import { Types } from './store/types';
 
 export default {
 	name: 'App',
+	data: () => ({
+		drawer: false,
+	}),
 	components: {
 	},
 	methods: {
